9e50d44e141a404468118ec050e376c64fa1fc59
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-16 10:46:57 +0000

    add code of views

---
+++ views.php
@@ -0,0 +1,9 @@
+<!DOTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>{% block title%}welcome!{% endblock%}</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/
+      2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%229%22>Rond</text></svg>">
+  </head>
+</html>
