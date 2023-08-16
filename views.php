f3d1fcaefe3bdc69c2e393df098958e77d962395
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-16 12:06:52 +0000

    add views html

--- views.php
+++ views.php
@@ -5,5 +5,18 @@
     <title>{% block title%}welcome!{% endblock%}</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/
       2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%229%22>Rond</text></svg>">
+    {# Run `composer require symfony/webpack-encore-bundle` to start using Synfony UX #}
+    {% block stylesheets %}
+    {{ encore_entry_link_tags('app')}}
+    {% endblock %}
+    
+    {% block javascripts %}
+    {{ encore_entry_script_tags('app')}}
+    {% endblock %}
   </head>
+  <body>
+    {% block body %}
+    <h1>Votre numéro de porte-boheur est le {{ number }}</h1>
+    {% endblock %}
+  </body>
 </html>
