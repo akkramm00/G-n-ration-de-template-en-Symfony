9610f8a57bfe84d61c065e5ecccb32eaa0f57f94
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-16 08:17:17 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Affichage de la première page HTML avec TWIG</title>
+  </head>
+  <body>
+<h2>HTML ave TWIG</h2>
+<p>
+  Si vous voulez retourner du HTML depuis votre controller, vous voudrez probablement rendre un modèle de page .On appelle ce modèle de page "template".
+  Heureusement , Symfony est livré avec un moteur de template appelé "TWIG" : un langage templating minimal, puissant et assez facile à prendre en main.
+</p>
+    <h2>Définition d'un système de template</h2>
+
+    <p>
+      Un système de tem^plate permet de gébérer des pages HTML classiques tout en injectant des variables provenant de  Symfony. Nous générnos alors des "vues". Ce sont des "views"de la términologie "MVC". Ce mécanisme permet une approche tès puissante de modèles de pages.
+    </p>
+
+    <h3>Méthode de génération de template</h3>
+    <p>
+      
+    </p>
+  </body>
+</html>
